bd2b096a5e71fbc91a23298efe27477e630c4e92
Myeonghwan Yu <gmt1361@gmail.com>
2024-02-02 07:25:22 +0900

    api 작성완료

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import UsersRouter from './routers/users.router.js';
+import PostRouter from './routers/documents.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +9,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api', [UsersRouter]);
+app.use('/api', [UsersRouter, PostRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트로 서버가 열렸어요!');
